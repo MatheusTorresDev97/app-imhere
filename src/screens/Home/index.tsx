415de790a1458eec974f6ce57e420566c1d56b9a
Matheus <matheustorresdev@gmail.com>
2023-12-12 20:53:23 -0300

    criando o form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,26 +3,27 @@ import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
 const Home = () => {
-
-    const handleParticipantAdd = () => {
-        console.log('Você clicou no botão de Adicionar !')
-    }
+  const handleParticipantAdd = () => {
+    console.log("Você clicou no botão de Adicionar !");
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
       <Text style={styles.eventDate}>Terça, 12/12/2023</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Nome do participante"
-        placeholderTextColor="#6b6b6b"
-      />
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6b6b6b"
+        />
 
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText} onPress={handleParticipantAdd}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText} onPress={handleParticipantAdd}>
             +
-        </Text>
-      </TouchableOpacity>
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
